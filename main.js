1c2f8865c1290277256c0a37177ae171fde756dc
Ajeet Shah <ajeets.shah@hotmail.com>
2017-02-15 23:27:28 +0530

    Filter

--- main.js
+++ main.js
@@ -3,6 +3,43 @@ var myApp = angular.module('myApp', []);
 myApp.factory('Data', function() {
 	return {message: "I am a data from service"};
 });
+myApp.factory('Avengers', function() {
+	var Avengers = {};
+	Avengers.cast = [
+		{
+			name: "Robert Downey1",
+			character: "Iron Man1"
+		},	
+		{
+			name: "Robert Downey2",
+			character: "iron man2"
+		},	
+		{
+			name: "Robert Downey3",
+			character: "Iron Man3"
+		},	
+		{
+			name: "Robert Downey4",
+			character: "Iron Man4"
+		},	
+		{
+			name: "Chris Ivan",
+			character: "Steve Rogers"
+		},	
+		{
+			name: "Mark Ruffalo",
+			character: "The Hulk"
+		},	
+		{
+			name: "Clark Gregg",
+			character: "Agent Phil Coulson"
+		}
+
+
+
+	];
+	return Avengers;
+});
 
 myApp.filter('reverse', function(Data) {
 	return function(text){
@@ -13,8 +50,10 @@ myApp.filter('reverse', function(Data) {
 myApp.controller('firstCtrl', function($scope, Data){
 	$scope.data = Data;
 });
-
 myApp.controller('secondCtrl', function($scope, Data){
 	$scope.data = Data;
+});
+myApp.controller('AvengersCtrl', function($scope, Avengers) {
+	$scope.avengers = Avengers;
 });
 
