067c5899aed7482a90d0576342e9003ca8fc5097
Ajeet Shah <ajeets.shah@hotmail.com>
2017-02-21 22:37:04 +0530

    Basic behaviors

---
+++ behaviour/main.js
@@ -0,0 +1,33 @@
+var app = angular.module('behaviorApp', []);
+
+app.directive('enter', function() {
+	return {
+		restrict: "A",
+		template: "<div><p>I am made in enter directive</p></div>",
+		link: function() {
+			console.debug("enter directive loaded");
+
+			var hello = function () {
+				console.debug("Hello World");
+			}
+
+			hello();
+		}
+	};
+});
+
+app.directive('enter2', function() {
+	return function(scope, element, attrs) {
+		element.bind("mouseenter", function() {
+			console.debug("I am inside of you!");
+		});
+	};
+});
+
+app.directive("leave", function() {
+	return function (scope, element, attrs) {
+		element.bind("mouseleave", function() {
+			console.debug("I am not outside of you!!");
+		});
+	}
+});
